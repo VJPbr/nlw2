ef0d179e5bf6e3c8b4e0e0e51b85b23a28fe77aa
VJPbr <victor.jp97@hotmail.com>
2020-08-03 23:59:22 -0300

    v1.0

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css'
+
+function TeacherItem () {
+  return(
+  <article className="teacher-item">
+    <header>
+      <img src="https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4" alt="Diego Fernandes" />
+      <div>
+        <strong>Diego Fernandes</strong>
+        <span>Química</span>
+      </div>
+    </header>
+
+    <p>
+      Texto de teste!
+            <br /><br />
+            Mais uma parte do texto de teste!
+          </p>
+
+    <footer>
+      <p>
+        Preço/hora
+              <strong>R$ 20,00</strong>
+      </p>
+      <button type="button">
+        <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+    </footer>
+  </article>
+  );
+};
+
+export default TeacherItem;
